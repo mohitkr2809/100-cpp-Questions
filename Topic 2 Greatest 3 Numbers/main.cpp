5349c19c98b8da926e35fab0626e7ceea3cc0727
mohitkr2809 <mohitkrmnm282003@gmail.com>
2026-07-09 12:09:50 +0530

    code executed successfully

--- Topic 2 Greatest 3 Numbers/main.cpp
+++ Topic 2 Greatest 3 Numbers/main.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-    int a = 10,
-    b = 20,
-    c = 30;
-
+int main()
+{
+    int a = 10, b = 20, c = 30;
     cout << "The Greatest Among Three Numbers is : ";
-
-    if (a >=b && a >=c) {
+    if (a >= b && a >= c) {
         cout << a << endl;
-    } else if (b >=a && b >= c) {
+    }
+    else if (b >= a && b >= c) {
         cout << b << endl;
-    } else {
+    }
+    else {
         cout << c << endl;
     }
-
     return 0;
 }
